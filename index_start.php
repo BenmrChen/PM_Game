641c19e6c43074bf92fd6c573519a17ea9171877
mingren.chen16 <mingren.chen16@gmail.com>
2019-04-11 11:32:37 +0800

    modify index_start.php

--- index_start.php
+++ index_start.php
@@ -7,7 +7,7 @@
 <h1><取名時刻></h1>
 <hr>
 <p>歡迎進入大人的世界</p>
-<p>經歷過這精神與時光之屋般的5天，相信你會徹底脫胎換骨，成為專業PM的日子是指日可待:)</p>
+<p>經歷過這精神與時光之屋般的3天，相信你會徹底脫胎換骨，成為專業PM的日子是指日可待:)</p>
 <p>請輸入您的大名:</p>
 <form action="role_selection.php" method="post">
   <input type="text" name="role_name" placeholder="未來的專業PM名為... " style="width:150px; height:30px;">
